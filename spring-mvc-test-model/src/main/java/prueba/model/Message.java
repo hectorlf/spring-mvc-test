f491dbf620527f2415728dd228e1651e4d04c27c
hector <drht@lycos.com>
2014-02-23 00:11:41 +0100

    - Se separan las clases del modelo a un subproyecto para que no haya problemas con el static weaving de eclipselink (que muy listo no es, el pobre) - La construcción se controla con un multiproyecto de Gradle

---
+++ spring-mvc-test-model/src/main/java/prueba/model/Message.java
@@ -0,0 +1,38 @@
+package prueba.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+
+@Entity
+@Table(name="messages")
+public class Message {
+
+	@Id
+	@Column(name="id")
+	private Integer id;
+	
+	@NotNull
+	@Column(name="text")
+	private String text;
+
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getText() {
+		return text;
+	}
+
+	public void setText(String text) {
+		this.text = text;
+	}
+
+}
